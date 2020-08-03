e259ad32fda17856de8dc75cae57e14f015b0344
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-03 06:31:10 +0530

    Added: Regex pattern for validating email

--- userRegistration.js
+++ userRegistration.js
@@ -1,18 +1,21 @@
 const userInput = require('prompt-sync')();
 let regexName = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
-
+let emailPattern = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
 
 function checkValidUserInput( userInput, regexPattern){
-		if( regexName.test(userInput))
+		if( regexPattern.test(userInput))
 			console.log("Valid")
 		else
 			console.log("invalid")
 }
 
-let firstName = userInput("Enter First Name");
+let firstName = userInput("Enter First Name  ");
 checkValidUserInput( firstName, regexName);
 
-let lastName = userInput("Enter last name");
+let lastName = userInput("Enter last name  ");
 checkValidUserInput( lastName, regexName);
+
+let email = userInput("Enter email  ");
+checkValidUserInput( email, emailPattern);
 
 
